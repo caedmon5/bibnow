af927f35b03f1577c718feed3e13795af1af060e
Dan O'Donnell <daniel.odonnell@uleth.ca>
2025-08-23 15:57:33 -0600

    moved readme to higher directory

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+ZOTERO_API_KEY = "sM3L2EqpXtILF9QJ3aabtZ82"
+ZOTERO_USER_ID = "632779"      # Replace with your Zotero numeric ID
+ZOTERO_USERNAME = "daniel.odonnell"          # For public links
+
+# Obsidian path on this machine
+OBSIDIAN_VAULT_PATH = "/home/dan/wealtheow/LN Literature Notes"
+
+# Default CSL style (must exist in csl/ directory)
+BIBLIO_STYLE = "/home/dan/git/biblio_mobile_pipeline_v1/csl/chicago-author-date.csl"
+
+ZOTERO_GROUP_ID = "6069337"
+ZOTERO_GROUP_SLUG = "bibnow"
